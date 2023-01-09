23a9f8ad547dd48d645e93bea382326942e8e5bd
Joshua Tint <tintjosh@gmail.com>
2023-01-09 13:54:56 -0700

    refactor to make movingActors

---
+++ src/MovingActor.java
@@ -0,0 +1,57 @@
+public class MovingActor extends Actor {
+    int dx = 0;
+    int dy = 0;
+    int maxSpeed;
+
+    public MovingActor(Point location, Environment environment, int maxSpeed) {
+        super(location, environment);
+
+        this.maxSpeed = maxSpeed;
+    }
+
+
+    int constrainedAdd(int a, int b) {
+        return Math.min(Math.max(a+b, -maxSpeed), maxSpeed);
+    }
+
+    void accelRight() {
+        dx = constrainedAdd(dx, 2);
+
+    }
+    void accelLeft() {
+        dx = constrainedAdd(dx, -2);
+
+    }
+    void accelFront() {
+        dy = constrainedAdd(dy, 2);
+
+    }
+    void accelBack() {
+        dy = constrainedAdd(dy, -2);
+    }
+    void deaccelX() {
+        dx /= 2;
+    }
+    void deaccelY() {
+        dy /= 2;
+    }
+
+    @Override
+    void move() {
+        location.offsetY += dy;
+        location.offsetX += dx;
+
+        if (dy < 0 && !canMoveBack()) {
+            location.offsetY -= dy;
+        }
+        if (dy > 0 && !canMoveFront()) {
+            location.offsetY -= dy;
+        }
+        if (dx > 0 && !canMoveRight()) {
+            location.offsetX -= dx;
+        }
+        if (dx < 0 && !canMoveLeft()) {
+            location.offsetX -= dx;
+        }
+    }
+}
--- src/Player.java
+++ src/Player.java
@@ -1,78 +1,55 @@
 import java.awt.event.KeyEvent;
 
-public class Player extends Actor  {
+public class Player extends MovingActor  {
 
     Direction direction = Direction.DOWN;
     boolean moving = false;
-    private int dy = 0;
-    private int dx = 0;
-    private final int maxSpeed = 3;
 
     private int flashingBeginFrame = -100;
 
     public Player(Point location, Environment e) {
-        super(location, e);
+        super(location, e, 3);
 
         imageFetcher = () -> moving ? Images.getImage("mario"+direction.charCode()+Application.frameNumber()) : Images.getImage("mario"+direction.charCode()+"2");
-    }
-
-    int constrainedAdd(int a, int b) {
-        return Math.min(Math.max(a+b, -maxSpeed), maxSpeed);
     }
 
     @Override
     void move() {
         if (Application.keyData.getIsPressed(KeyEvent.VK_UP)) {
             direction = Direction.UP;
-            dy = constrainedAdd(dy, -2);
+            accelBack();
         } else if (Application.keyData.getIsPressed(KeyEvent.VK_DOWN)) {
             direction = Direction.DOWN;
-            dy = constrainedAdd(dy, 2);
+            accelFront();
         } else {
-            dy /= 2;
+            deaccelY();
         }
         if (Application.keyData.getIsPressed(KeyEvent.VK_LEFT)) {
             direction = Direction.LEFT;
-            dx = constrainedAdd(dx, -2);
+            accelLeft();
         } else if (Application.keyData.getIsPressed(KeyEvent.VK_RIGHT)) {
             direction = Direction.RIGHT;
-            dx = constrainedAdd(dx, 2);
+            accelRight();
         } else {
-            dx /= 2;
+            deaccelX();
         }
 
-
-        location.offsetY += dy;
-        location.offsetX += dx;
-
-        if (dy < 0 && !canMoveBack()) {
-            location.offsetY -= dy;
-        }
-        if (dy > 0 && !canMoveFront()) {
-            location.offsetY -= dy;
-        }
-        if (dx > 0 && !canMoveRight()) {
-            location.offsetX -= dx;
-        }
-        if (dx < 0 && !canMoveLeft()) {
-            location.offsetX -= dx;
-        }
-
+        super.move();
 
         if (Application.keyData.getIsPressed(KeyEvent.VK_X) && Application.frameCount%2==0 && environment.hud.waterLevel > 0) {
             Point loc = new Point(location.x, location.y, location.z, location.offsetX, location.offsetY, 16);
-            int dx = switch (direction) {
-                case LEFT -> -5;
-                case RIGHT -> 5;
+            int wdx = switch (direction) {
+                case LEFT -> -5+dx;
+                case RIGHT -> 5+dx;
                 default -> 0;
             };
-            int dy = switch (direction) {
-                case UP -> -5;
-                case DOWN -> 5;
+            int wdy = switch (direction) {
+                case UP -> -5+dy;
+                case DOWN -> 5+dy;
                 default -> 0;
             };
 
-            environment.addActor(new ActorLibrary.WaterDrop(loc, environment, dx, dy));
+            environment.addActor(new ActorLibrary.WaterDrop(loc, environment, wdx, wdy));
 
             environment.hud.waterLevel-=.5;
             environment.hud.waterLevel = Math.max(0, environment.hud.waterLevel);
